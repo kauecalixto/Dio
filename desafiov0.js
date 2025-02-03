db8533d85c0d83bc537f412e1a97855288695eb6
Kaue_ocalixto <kaue.santos.calixto@gmail.com>
2025-02-03 17:32:48 -0300

    Desafio 2

---
+++ desafiov0.js
@@ -0,0 +1,33 @@
+ 
+function calculadNivel(derrotas , vitorias , nome, nivel){
+    const saldoVitorias = vitorias - derrotas;
+    let nivel;
+ 
+    if (xp < 1000) {
+        nivel = "Ferro";
+     } else if (xp >= 1001 && xp <= 2000) {
+        nivel = "Bronze";
+     } else if (xp >= 2001 && xp <= 5000){
+        nivel = "Prata";
+     } else if (xp >= 5001 && xp <= 7000){
+        nivel = "Ouro";
+     } else if (xp >= 7001 && xp <= 8000){
+        nivel = "Platina";
+     } else if (xp >= 8001 && xp <= 9000){
+        nivel = "Ascendente";
+     } else if (xp >= 9001 && xp <= 10000){
+        nivel = "Imortal";
+     } else {
+        nivel = "Radiante";
+     } 
+
+     return { saldoVitorias, nivel };
+
+}
+
+
+const vitorias = parseInt(prompt("Digite o número de vitórias:"));
+const derrotas = parseInt(prompt("Digite o número de derrotas:"));
+
+const resultado = calcularNivel(vitorias, derrotas);
+alert(`O Herói tem um saldo de ${resultado.saldoVitorias} e está no nível ${resultado.nivel}.`);
